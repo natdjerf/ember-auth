d433ccade163a370fecb0522a9f03032ad0645c6
natdjerf <nadjerf@gmail.com>
2016-06-03 14:59:30 -0400

    Class Notes
    add notes from class 6/3

--- app/change-password/route.js
+++ app/change-password/route.js
@@ -7,15 +7,20 @@ export default Ember.Route.extend({
   actions: {
     changePassword (passwords) {
       this.get('auth').changePassword(passwords)
-      .then(() => this.get('auth').signOut())
-      .then(() => this.transitionTo('sign-in'))
+      // if you prefer to have your user sign out after changing password, uncomment out below
+      // & direct the user back to sign-in page
+      // transition to whereever is appropriate (home? previous page?)
+      
+      // .then(() => this.get('auth').signOut())
+      // .then(() => this.transitionTo('sign-in'))
+      .then(() => this.transitionTo('application'))
       .then(() => {
         this.get('flashMessages')
         .success('Successfully changed your password!');
       })
-      .then(() => {
-        this.get('flashMessages').warning('You have been signed out.');
-      })
+      // .then(() => {
+      //   this.get('flashMessages').warning('You have been signed out.');
+      // })
       .catch(() => {
         this.get('flashMessages')
         .danger('There was a problem. Please try again.');
--- app/sign-up/route.js
+++ app/sign-up/route.js
@@ -7,8 +7,11 @@ export default Ember.Route.extend({
   actions: {
     signUp (credentials) {
       this.get('auth').signUp(credentials)
+      // go to the auth folder > signUp method & pass credentials
       .then(() => this.get('auth').signIn(credentials))
+      // automatically signIn after signUp (using the same credentials)
       .then(() => this.transitionTo('application'))
+      // this brings you back the the url for the application
       .then(() => {
         this.get('flashMessages')
         .success('Successfully signed-up! You have also been signed-in.');
